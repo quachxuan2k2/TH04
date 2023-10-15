34a96b2da90fb831d948a42ba762df14b0c2dfb6
xuandebug <98002677+xuandebug@users.noreply.github.com>
2023-10-15 22:41:57 +0700

    card file

--- lib/Card.dart
+++ lib/Card.dart
@@ -1,25 +1,23 @@
-
-
 import 'package:flutter/material.dart';
 
-class ViewCard{
+class News{
   DateTime? dateTime;
   String? title;
   String? text;
-  ViewCard([this.dateTime,this.title,this.text]);
+  News([this.dateTime,this.title,this.text]);
 }
-class _ViewCardState extends State<CardWiget> {
-  List<ViewCard> listCard=[
-    ViewCard(DateTime(2023,10,11),"Mass shooting in Atlanta",
-         "Lorem ipsum dolor sit amet, consectetur adipiscing elit Proin sit ame"),
-    ViewCard(DateTime(2023,10,11),"Carnival clown found drunk in Misisippi",
-         "Lorem ipsum dolor sit amet, consectetur adipiscing elit Proin sit ame"),
-    ViewCard(DateTime(2023,10,11),"Walrus found in family pool in Florida",
-       "Lorem ipsum dolor sit amet, consectetur adipiscing elit Proin sit ame"),
+class _CardWidgetState extends State<CardWidget> {
+  List<News> listCard=[
+    News(DateTime(2023,10,11),"Mass shooting in Atlanta",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit Proin sit ame"),
+    News(DateTime(2023,10,11),"Carnival clown found drunk in Misisippi",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit Proin sit ame"),
+    News(DateTime(2023,10,11),"Walrus found in family pool in Florida",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit Proin sit ame"),
   ];
   @override
   Widget build(BuildContext context) {
-    List<Widget> listWiget = listCard.map((e)=>MyCard(e)).toList();
+    List<Widget> listWiget = listCard.map((e)=>NewsCard(e)).toList();
     return Scaffold(
       appBar: AppBar(
         title: Text('ViewCard'),
@@ -31,16 +29,16 @@ class _ViewCardState extends State<CardWiget> {
 
   }}
 
-class CardWiget extends StatefulWidget {
+class CardWidget extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return _ViewCardState();
+    return _CardWidgetState();
   }
 }
 
-class MyCard extends StatelessWidget{
-  ViewCard? viewCard;
-  MyCard([this.viewCard]);
+class NewsCard extends StatelessWidget{
+  News? news;
+  NewsCard([this.news]);
   @override
   Widget build(BuildContext context) {
     return Padding(padding: EdgeInsets.all(15.0),
@@ -50,16 +48,16 @@ class MyCard extends StatelessWidget{
           children: [
             Image.asset('img/bbc.png'),
             Padding(padding: EdgeInsets.all(15.0),
-              child: Text('${viewCard?.dateTime?.day}/${viewCard?.dateTime?.month}'
-                  '/${viewCard?.dateTime?.year}',
+              child: Text("${news?.dateTime?.day}/${news?.dateTime?.month}"
+                  "/${news?.dateTime?.year}",
               ),
             ),
             Padding(padding: EdgeInsets.only(top: 15.0),
-                child: Text('${viewCard?.title}',
-                  style: const TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold))
+                child: Text('${news?.title}',
+                  style: const TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold),)
             ),
             Padding(padding: EdgeInsets.only(top:15.0),
-              child: Text('${viewCard?.text}'),
+              child: Text('${news?.text}'),
             ),
             Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
@@ -69,13 +67,17 @@ class MyCard extends StatelessWidget{
               ],)
           ],
         ),
+
       ),
     );
+
   }
   void onPressedShare() {
   }
+
   void onPressedBookmark() {
   }
+
   void onPressedLink() {
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'Card.dart';
 import 'Flexible.dart';
 import 'GestureDetector.dart';
+//import 'Card2.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: GuestureWidget()
+    home: CardWidget()
+    //NewsCard()
   ),
   );
 }
